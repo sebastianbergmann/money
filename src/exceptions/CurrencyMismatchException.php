ee779569f11ecb52697afbf816b4826ca1a93892
Sebastian Bergmann <sb@sebastian-bergmann.de>
2013-07-18 15:03:49 +0200

    This is also an InvalidArgumentException

--- src/exceptions/CurrencyMismatchException.php
+++ src/exceptions/CurrencyMismatchException.php
@@ -49,7 +49,7 @@ namespace SebastianBergmann\Money
      * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
      * @link       http://www.github.com/sebastianbergmann/money
      */
-    class CurrencyMismatchException extends \RuntimeException implements Exception
+    class CurrencyMismatchException extends InvalidArgumentException
     {
     }
 }
