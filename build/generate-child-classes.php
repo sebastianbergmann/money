3d3a9d5b4a5f27c5798c743e993aa8d74e07bc0f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2013-11-13 10:35:42 +0100

    TRY is not a valid class name

--- build/generate-child-classes.php
+++ build/generate-child-classes.php
@@ -8,6 +8,10 @@ $attribute = $class->getProperty('currencies');
 $attribute->setAccessible(true);
 
 foreach (array_keys($attribute->getValue()) as $currencyCode) {
+    if ($currencyCode == 'TRY') {
+        continue;
+    }
+
     file_put_contents(
         __DIR__ . '/../src/currency/' . $currencyCode . '.php',
         str_replace(
