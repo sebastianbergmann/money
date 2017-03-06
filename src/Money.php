3bf9b9bba252d201e91d5e7e0d5e6ec931108de8
HSarode-Compumatrice <hsarode@edrnet.com>
2017-03-06 21:10:59 +0530

    123 changed

--- src/Money.php
+++ src/Money.php
@@ -23,7 +23,8 @@ namespace SebastianBergmann\Money;
  * @see        http://martinfowler.com/bliki/ValueObject.html
  * @see        http://martinfowler.com/eaaCatalog/money.html
  */
-class Money implements \JsonSerializable
+//Hello
+class Money123 implements \JsonSerializable
 {
     /**
      * @var integer
