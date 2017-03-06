a77715344f0abe7efc3af3e246fa50e25233672c
HSarode-Compumatrice <hsarode@edrnet.com>
2017-03-06 16:25:13 +0530

    Update Currency.php

--- src/Currency.php
+++ src/Currency.php
@@ -9,7 +9,7 @@
  */
 
 namespace SebastianBergmann\Money;
-//dvjkdjvnkjdvnkjfnvkj
+//dvjkdjv
 /**
  * Value Object that represents a currency.
  *
@@ -23,7 +23,7 @@ namespace SebastianBergmann\Money;
  * @see        http://docs.oracle.com/javase/7/docs/api/java/util/Currency.html
  * @SuppressWarnings(PHPMD.ExcessiveClassLength)
  */
-class Currency1234
+class Currency
 {
     /**
      * @var array
