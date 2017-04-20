785bb0a70c016c5c5d0881c4a1e7e071b252aed2
HSarode-Compumatrice <hsarode@edrnet.com>
2017-04-20 16:44:48 +0530

    Update Money.php

--- src/Money.php
+++ src/Money.php
@@ -24,7 +24,7 @@ namespace SebastianBergmann\Money;
  * @see        http://martinfowler.com/eaaCatalog/money.html
  */
 //Hellofhfdjbkjfbdkjfbjdbfjkdebfkjdbkjfbdkjfbdkjbjf1234
-class Money123 implements \JsonSerializable
+class Money implements \JsonSerializable
 {
     /**
      * @var integer
