9fb6c17234738c04555e0f36faf5974349dbbb48
HSarode-Compumatrice <hsarode@edrnet.com>
2017-04-24 12:56:39 +0530

    added comment to line no. 10

--- src/Currency.php
+++ src/Currency.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
+//Added Comment to Number 10 here
 namespace SebastianBergmann\Money;
 
 /**
